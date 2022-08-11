a0dd67905d8023b76751b2ac87609fa3e5c9f19c
Piyus <pk4096782gmail.com>
2022-08-11 09:52:02 +0530

    completed

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,51 +1,43 @@
 const express = require('express');
 const router = express.Router();
+  // -write an api which gives the missing number in an array of integers starting from 1….e.g [1,2,3,5,6,7] : 4 is missing
+ // Your route code will look like this
+ router.get("/sol1", function(req, res) {
+    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+  let arr= [1,2,3,5,6,7]
 
-let players =
-   [
-       {
-           "name": "manish",
-           "dob": "1/1/1995",
-           "gender": "male",
-           "city": "jalandhar",
-           "sports": [
-               "swimming"
-           ]
-       },
-       {
-           "name": "gopal",
-           "dob": "1/09/1995",
-           "gender": "male",
-           "city": "delhi",
-           "sports": [
-               "soccer"
-           ],
-       },
-       {
-           "name": "lokesh",
-           "dob": "1/1/1990",
-           "gender": "male",
-           "city": "mumbai",
-           "sports": [
-               "soccer"
-           ],
-       },
-   ]
+  let total = 0;
+  for (let i in arr) {
+      total += arr[i];
+  }
+  let lastDigit= arr.pop()
+  let consecutiveSum= lastDigit * (lastDigit+1) / 2
+  let missingNumber= consecutiveSum - total
+    res.send(JSON.stringify(missingNumber));
+});
 
-   router.post('/players', function (req, res) {
-    let playerName = req.body.name;
-    let newPlayer = req.body;
 
-    for(let i = 0; i<players.length;i++){
-       if(players[i].name == playerName){
-           return res.send("This player is already exist");
-        }}
-      players.push(newPlayer);
-      res.send(players)
-   })
-  
+
+router.get("/sol2", function(req, res) {
+
+let arr= [33, 34, 35, 37, 38]
+let len= arr.length
+
+let total = 0;
+for (let i in arr) {
+    total += arr[i];
+}
+
+let firstDigit= arr[0]
+let lastDigit= arr.pop()
+let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
+let missingNumber= consecutiveSum - total
+
+
+res.send(JSON.stringify(missingNumber));
+
+});
+
 module.exports = router;
-
-
 
 
